61236296aad86f85957b1f8e2ee5124b8b20a43a
dhanraj <dhanrajp3531@gmail.com>
2019-04-28 00:57:17 +0530

    adding middleware settings trial 1

--- work_hours/work_hours/settings/base.py
+++ work_hours/work_hours/settings/base.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #my custom middleware
+    # 'swipe.middleware.ExceptionReasonFinder'
 ]
 
 ROOT_URLCONF = 'work_hours.urls'
